ebf2adaa5ff5a138bbcbb4b7608cf84284efb1e7
Natasha Koller <kollernatasha@gmail.com>
2022-01-06 16:06:53 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,34 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch('http://localhost:3000/toys')
+  .then(response => response.json())
+  .then(response => {
+    console.log(response)
+	    response.forEach((toy) => {
+        addToyInfo(toy)
+    })
+  })
+})
+
+function addToyInfo(toy){
+  let toyCollection = document.getElementById("toy-collection")
+  toyCollection.innerHTML += `
+  <div class ="card">
+    <h2>${toy.name}</h2>
+    <img src=${toy.image} class="toy-avatar" />
+    <p>${toy.likes} Likes</p>
+    <button class="like-btn" id=${toy.id}>Like ❤️</button>
+  </div>
+  `
+  console.log(toy)
+}
+
+// // addEventListener('submit',____)so that when the form is submitted 
+// // the new toy is persisted to the database and a new card showing the 
+// // toy is added to the DOM
+// // addEventListener('click', likeToy) When the button is clicked 
+// // the number of likes should be updated in the database 
+// // and the updated information should be rendered to the dom
